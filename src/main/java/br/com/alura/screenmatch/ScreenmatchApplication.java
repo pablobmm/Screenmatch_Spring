fcfd751025ef9363118c64e0a0ccab79fbf9be52
Pablo <148821043+pablobmm@users.noreply.github.com>
2026-01-06 23:39:18 -0300

    feat: Consumindo e desserializando dados da série

---
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,22 @@
+package br.com.alura.screenmatch;
+
+import br.com.alura.screenmatch.service.ConsumoApi;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		var consumoApi = new ConsumoApi();
+		var json = consumoApi.obterDados("chave");
+		System.out.println(json);
+
+	}
+}
