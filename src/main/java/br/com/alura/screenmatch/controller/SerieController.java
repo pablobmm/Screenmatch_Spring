a0c4d00fe4e325364fd9e06fd419042f1c3eb6cd
Pablo <148821043+pablobmm@users.noreply.github.com>
2026-02-26 18:18:53 -0300

    repositório da serie adicionado

--- src/main/java/br/com/alura/screenmatch/controller/SerieController.java
+++ src/main/java/br/com/alura/screenmatch/controller/SerieController.java
@@ -1,12 +1,21 @@
 package br.com.alura.screenmatch.controller;
 
+import br.com.alura.screenmatch.model.Serie;
+import br.com.alura.screenmatch.repository.SerieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class SerieController {
+
+    @Autowired
+    private SerieRepository repositorio;
+
     @GetMapping("/series")
-    public String obterSeries(){
-        return "As séries irão ser lançadas aqui";
+    public List<Serie> obterSeries(){
+        return repositorio.findAll();
     }
 }
