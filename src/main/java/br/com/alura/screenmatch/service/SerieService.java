07198c30b06189817b982117a6d2a0e7e1d955b9
Pablo <148821043+pablobmm@users.noreply.github.com>
2026-03-04 16:32:19 -0300

    feat: episodios mais recentes

--- src/main/java/br/com/alura/screenmatch/service/SerieService.java
+++ src/main/java/br/com/alura/screenmatch/service/SerieService.java
@@ -32,7 +32,7 @@ public class SerieService {
     }
 
     public List<SerieDTO> obterLancamento() {
-        return converteDados(repositorio.findTop5ByOrderByEpisodiosDataLancamentoDesc());
+        return converteDados(repositorio.encontrarEpisodiosMaisRecentes());
     }
 
     public SerieDTO obterPorId(Long id) {
